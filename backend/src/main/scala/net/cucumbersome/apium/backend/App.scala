7cdde5c5e28fd06e5575172e213751beab4b1515
Michał Gutowski <michal@cucumbersome.net>
2019-04-09 22:01:07 +0200

    Cors added

--- backend/src/main/scala/net/cucumbersome/apium/backend/App.scala
+++ backend/src/main/scala/net/cucumbersome/apium/backend/App.scala
@@ -10,6 +10,7 @@ import org.http4s.server.blaze.BlazeServerBuilder
 import scalaz.zio.interop.catz._
 import scalaz.zio.interop.catz.implicits._
 import scalaz.zio.{Task, ZIO}
+import org.http4s.server.middleware._
 
 object App extends CatsApp {
 
@@ -21,7 +22,7 @@ object App extends CatsApp {
   )
 
   override def run(args: List[String]): ZIO[backend.App.Environment, Nothing, Int] = {
-    val router = Router("/" -> peopleService.service).orNotFound
+    val router = Router("/" -> CORS(peopleService.service)).orNotFound
     BlazeServerBuilder[Task]
       .bindHttp(8080, "0.0.0.0")
       .withHttpApp(router)
